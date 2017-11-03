6e01d4fda9201edbab43fe676107412df03e552c
mahesh <maheshchinnala@gmail.com>
2017-11-03 12:04:11 +0530

    page changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mahesh
                 </div>
 
                 <div class="links">
